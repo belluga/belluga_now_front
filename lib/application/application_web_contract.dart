beba941ee54177ac3e7acf85962b9d46380fefd0
eltonmorais <elton@marketinglucrativo.com>
2025-07-10 08:19:58 -0300

    :hammer: simplify setup of URL strategy.

--- lib/application/application_web_contract.dart
+++ lib/application/application_web_contract.dart
@@ -1,5 +1,4 @@
 import 'package:unifast_portal/application/application_contract.dart';
-import 'package:unifast_portal/application/helpers/url_strategy/url_strategy.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:video_player_web_hls/video_player_web_hls.dart';
 
@@ -8,7 +7,7 @@ abstract class ApplicationWebContract extends ApplicationContract {
 
   @override
   Future<void> initialSettingsPlatform() async {
-    setupUrlStrategy();
+    setUrlStrategy(PathUrlStrategy());
     VideoPlayerPluginHls.registerWith(webPluginRegistrar);
   }
 }
