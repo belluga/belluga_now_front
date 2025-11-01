1c54aed4fdaac588f34fb8096d1ead9adef679d6
eltonmorais <elton@marketinglucrativo.com>
2025-11-01 15:17:44 -0300

    feat(map): add mock HTTP service with configurable latency for POI queries

---
+++ lib/infrastructure/services/http/mock_http_service.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+import 'package:belluga_now/infrastructure/services/dal/datasources/mock_poi_database.dart';
+import 'package:belluga_now/infrastructure/services/dal/datasources/poi_query.dart';
+import 'package:belluga_now/infrastructure/services/dal/dto/map/city_poi_dto.dart';
+
+class MockHttpService {
+  MockHttpService({
+    required MockPoiDatabase database,
+    Duration latency = const Duration(milliseconds: 350),
+  })  : _database = database,
+        _latency = latency;
+
+  final MockPoiDatabase _database;
+  final Duration _latency;
+
+  Future<List<CityPoiDTO>> getPois(PoiQuery query) async {
+    await Future<void>.delayed(_latency);
+    return _database.findPois(query: query);
+  }
+}
