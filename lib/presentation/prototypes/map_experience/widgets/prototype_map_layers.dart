0f40f2c4d04d83dc7c24c441565198dec285757e
Elton <elton@bellugasolutions.com.br>
2025-11-09 21:12:51 -0300

    Engaging Markers are back

--- lib/presentation/prototypes/map_experience/widgets/prototype_map_layers.dart
+++ lib/presentation/prototypes/map_experience/widgets/prototype_map_layers.dart
@@ -1,6 +1,7 @@
 import 'package:belluga_now/domain/map/city_poi_model.dart';
 import 'package:belluga_now/domain/map/value_objects/city_coordinate.dart';
 import 'package:belluga_now/presentation/prototypes/map_experience/controllers/map_screen_controller.dart';
+import 'package:belluga_now/presentation/prototypes/map_experience/widgets/poi_marker_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:free_map/free_map.dart';
 import 'package:get_it/get_it.dart';
@@ -8,9 +9,11 @@ import 'package:stream_value/core/stream_value_builder.dart';
 
 class PrototypeMapLayers extends StatelessWidget {
   PrototypeMapLayers({super.key})
-      : _controller = GetIt.I.get<MapScreenController>();
+      : _controller = GetIt.I.get<MapScreenController>(),
+        _markerBuilder = const PoiMarkerBuilder();
 
   final MapScreenController _controller;
+  final PoiMarkerBuilder _markerBuilder;
 
   @override
   Widget build(BuildContext context) {
@@ -34,14 +37,17 @@ class PrototypeMapLayers extends StatelessWidget {
                         userCoordinate.latitude,
                         userCoordinate.longitude,
                       );
+                final poiMarkers = pois.map(
+                  (poi) => _markerBuilder.build(
+                    poi: poi,
+                    isSelected: selectedPoi?.id == poi.id,
+                    onTap: () => _controller.selectPoi(poi),
+                  ),
+                );
+
                 final markers = <Marker>[
                   if (userPoint != null) _buildUserMarker(userPoint),
-                  ...pois.map(
-                    (poi) => _buildPoiMarker(
-                      poi: poi,
-                      isSelected: selectedPoi?.id == poi.id,
-                    ),
-                  ),
+                  ...poiMarkers,
                 ];
 
                 return FmMap(
@@ -77,47 +83,6 @@ class PrototypeMapLayers extends StatelessWidget {
             Icons.my_location,
             size: 20,
             color: Colors.blueAccent,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Marker _buildPoiMarker({
-    required CityPoiModel poi,
-    required bool isSelected,
-  }) {
-    final point = LatLng(
-      poi.coordinate.latitude,
-      poi.coordinate.longitude,
-    );
-
-    final color = poi.isDynamic ? Colors.orangeAccent : Colors.white;
-    final borderColor = isSelected ? Colors.deepPurple : Colors.black54;
-
-    return Marker(
-      point: point,
-      width: isSelected ? 64 : 48,
-      height: isSelected ? 64 : 48,
-      child: GestureDetector(
-        onTap: () => _controller.selectPoi(poi),
-        child: AnimatedContainer(
-          duration: const Duration(milliseconds: 200),
-          decoration: BoxDecoration(
-            color: color,
-            shape: BoxShape.circle,
-            border: Border.all(color: borderColor, width: 3),
-            boxShadow: const [
-              BoxShadow(
-                color: Colors.black26,
-                blurRadius: 8,
-                offset: Offset(0, 4),
-              ),
-            ],
-          ),
-          child: Icon(
-            poi.isDynamic ? Icons.local_activity : Icons.place,
-            color: isSelected ? Colors.white : Colors.black87,
           ),
         ),
       ),
