d5c9886154dbed1ee7a50eb40236596ac3047a35
Elton <elton@bellugasolutions.com.br>
2025-11-23 23:26:13 -0300

    Implement invite functionality in SuaGaleraSection and enhance ImmersiveEventDetailScreen for better invite management

--- lib/presentation/tenant/schedule/screens/immersive_event_detail/immersive_event_detail_screen.dart
+++ lib/presentation/tenant/schedule/screens/immersive_event_detail/immersive_event_detail_screen.dart
@@ -2,6 +2,8 @@ import 'package:belluga_now/domain/schedule/event_model.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:belluga_now/application/router/app_router.gr.dart';
 import 'package:belluga_now/domain/invites/invite_model.dart';
+import 'package:belluga_now/domain/schedule/sent_invite_status.dart';
+import 'package:belluga_now/domain/schedule/invite_status.dart';
 import 'package:belluga_now/presentation/common/widgets/immersive_detail_screen/models/immersive_tab_item.dart';
 import 'package:belluga_now/presentation/common/widgets/immersive_detail_screen/immersive_detail_screen.dart';
 import 'package:belluga_now/presentation/tenant/schedule/screens/immersive_event_detail/controllers/immersive_event_detail_controller.dart';
@@ -60,96 +62,106 @@ class _ImmersiveEventDetailScreenState
             return StreamValueBuilder<List<InviteModel>>(
               streamValue: _controller.receivedInvitesStreamValue,
               builder: (context, receivedInvites) {
-                // Build tabs dynamically; loyalty tab moves to the end
                 final InviteModel? pendingInvite =
                     receivedInvites.isNotEmpty ? receivedInvites.first : null;
 
-                final tabs = <ImmersiveTabItem>[
-                  ImmersiveTabItem(
-                    title: 'O Rolê',
-                    content: EventInfoSection(event: event),
-                    footer: pendingInvite != null
-                        ? DynamicFooter(
-                            leftTitle: pendingInvite.inviterName ?? 'Convite',
-                            leftSubtitle: '${pendingInvite.hostName} te convidou',
-                            leftIcon: Icons.mail_outline,
-                            leftIconColor: Colors.purple,
-                            buttonText: 'Aceitar convite',
-                            buttonIcon: Icons.rocket_launch,
-                            buttonColor: const Color(0xFF9C27B0),
-                            onActionPressed: () =>
-                                _controller.acceptInvite(pendingInvite.id),
-                          )
-                        : null,
-                  ),
-                  ImmersiveTabItem(
-                    title: 'Line-up',
-                    content: LineupSection(event: event),
-                    footer: isConfirmed
-                        ? DynamicFooter(
-                            buttonText: 'Seguir todos os artistas',
-                            buttonIcon: Icons.star,
-                            buttonColor: const Color(0xFF6A1B9A),
-                            onActionPressed: () {
-                              // TODO: follow all artists
-                            },
-                          )
-                        : null,
-                  ),
-                  ImmersiveTabItem(
-                    title: 'O Local',
-                    content: LocationSection(event: event),
-                    footer: isConfirmed
-                        ? DynamicFooter(
-                            buttonText: 'Traçar Rota agora',
-                            buttonIcon: Icons.navigation,
-                            buttonColor: const Color(0xFF00ACC1),
-                            onActionPressed: () {
-                              // TODO: open maps
-                            },
-                          )
-                        : null,
-                    ),
-                  if (isConfirmed)
-                    ImmersiveTabItem(
-                      title: 'Ganhe Brindes',
-                      content: Align(
-                        alignment: Alignment.topCenter,
-                        child: UnconstrainedBox(
-                          alignment: Alignment.topCenter,
-                          constrainedAxis: Axis.horizontal,
-                          child: StreamValueBuilder(
-                            streamValue: _controller.missionStreamValue,
-                            onNullWidget: const SizedBox.shrink(),
-                            builder: (context, mission) {
-                              return MissionWidget(mission: mission);
-                            },
-                          ),
-                        ),
+                return StreamValueBuilder<Map<String, List<SentInviteStatus>>>(
+                  streamValue: _controller.sentInvitesByEventStreamValue,
+                  builder: (context, sentInvitesByEvent) {
+                    final sentForEvent =
+                        sentInvitesByEvent[event.id.value] ?? const [];
+
+                    final tabs = <ImmersiveTabItem>[
+                      ImmersiveTabItem(
+                        title: 'O Rolê',
+                        content: EventInfoSection(event: event),
+                        footer: pendingInvite != null
+                            ? DynamicFooter(
+                                leftTitle:
+                                    pendingInvite.inviterName ?? 'Convite',
+                                leftSubtitle:
+                                    '${pendingInvite.hostName} te convidou',
+                                leftIcon: Icons.mail_outline,
+                                leftIconColor: Colors.purple,
+                                buttonText: 'Aceitar convite',
+                                buttonIcon: Icons.rocket_launch,
+                                buttonColor: const Color(0xFF9C27B0),
+                                onActionPressed: () =>
+                                    _controller.acceptInvite(pendingInvite.id),
+                              )
+                            : null,
                       ),
-                      footer: null,
-                    ),
-                ];
+                      ImmersiveTabItem(
+                        title: 'Line-up',
+                        content: LineupSection(event: event),
+                        footer: isConfirmed
+                            ? DynamicFooter(
+                                buttonText: 'Seguir todos os artistas',
+                                buttonIcon: Icons.star,
+                                buttonColor: const Color(0xFF6A1B9A),
+                                onActionPressed: () {
+                                  // TODO: follow all artists
+                                },
+                              )
+                            : null,
+                      ),
+                      ImmersiveTabItem(
+                        title: 'O Local',
+                        content: LocationSection(event: event),
+                        footer: isConfirmed
+                            ? DynamicFooter(
+                                buttonText: 'Traçar Rota agora',
+                                buttonIcon: Icons.navigation,
+                                buttonColor: const Color(0xFF00ACC1),
+                                onActionPressed: () {
+                                  // TODO: open maps
+                                },
+                              )
+                            : null,
+                      ),
+                      if (isConfirmed)
+                        ImmersiveTabItem(
+                          title: 'Ganhe Brindes',
+                          content: Align(
+                            alignment: Alignment.topCenter,
+                            child: UnconstrainedBox(
+                              alignment: Alignment.topCenter,
+                              constrainedAxis: Axis.horizontal,
+                              child: StreamValueBuilder(
+                                streamValue: _controller.missionStreamValue,
+                                onNullWidget: const SizedBox.shrink(),
+                                builder: (context, mission) {
+                                  return MissionWidget(mission: mission);
+                                },
+                              ),
+                            ),
+                          ),
+                          footer: null,
+                        ),
+                    ];
 
-                const topBanner = null;
+                    const topBanner = null;
 
-                final footer = isConfirmed
-                    ? _buildInviteFooter(() => _openInviteFlow(event))
-                    : DynamicFooter(
-                        buttonText: 'Bóora! Confirmar Presença!',
-                        buttonIcon: Icons.celebration,
-                        buttonColor: const Color(0xFF9C27B0),
-                        onActionPressed: _controller.confirmAttendance,
-                      );
+                    final footer = isConfirmed
+                        ? _buildInviteFooter(
+                            () => _openInviteFlow(event), sentForEvent)
+                        : DynamicFooter(
+                            buttonText: 'Bóora! Confirmar Presença!',
+                            buttonIcon: Icons.celebration,
+                            buttonColor: const Color(0xFF9C27B0),
+                            onActionPressed: _controller.confirmAttendance,
+                          );
 
-                return ImmersiveDetailScreen(
-                  heroContent: ImmersiveHero(event: event),
-                  title: event.title.value,
-                  betweenHeroAndTabs: topBanner,
-                  tabs: tabs,
-                  // Don't auto-navigate, let user scroll naturally
-                  // initialTabIndex defaults to 0
-                  footer: footer,
+                    return ImmersiveDetailScreen(
+                      heroContent: ImmersiveHero(event: event),
+                      title: event.title.value,
+                      betweenHeroAndTabs: topBanner,
+                      tabs: tabs,
+                      // Don't auto-navigate, let user scroll naturally
+                      // initialTabIndex defaults to 0
+                      footer: footer,
+                    );
+                  },
                 );
               },
             );
@@ -202,35 +214,209 @@ class _ImmersiveEventDetailScreenState
   }
 }
 
-Widget _buildInviteFooter(VoidCallback onInviteFriends) {
+Widget _buildInviteFooter(
+  VoidCallback onInviteFriends,
+  List<SentInviteStatus> sentInvites,
+) {
+  final hasInvites = sentInvites.isNotEmpty;
   return DynamicFooter(
-    leftWidget: Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        CircleAvatar(
-          radius: 16,
-          backgroundColor: Colors.purple.withValues(alpha: 0.12),
-          child: const Icon(
-            Icons.rocket_launch,
-            color: Colors.purple,
-            size: 18,
+    leftWidget: !hasInvites
+        ? Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CircleAvatar(
+                radius: 16,
+                backgroundColor: Colors.purple.withValues(alpha: 0.12),
+                child: const Icon(
+                  Icons.rocket_launch,
+                  color: Colors.purple,
+                  size: 18,
+                ),
+              ),
+              const SizedBox(width: 8),
+              const Flexible(
+                child: Text(
+                  'Convide sua galera para ir com você.',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ],
+          )
+        : Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _OverlappedAvatars(invites: sentInvites),
+              const SizedBox(height: 4),
+              ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 220),
+                child: Text(
+                  _inviteSummary(sentInvites),
+                  style: const TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ],
           ),
-        ),
-        const SizedBox(width: 8),
-        const Flexible(
-          child: Text(
-            'Convide sua galera para ir com você.',
-            style: TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-        ),
-      ],
-    ),
     buttonText: 'BORA? Agitar a galera!',
     buttonIcon: Icons.rocket_launch,
     buttonColor: const Color(0xFF9C27B0),
     onActionPressed: onInviteFriends,
   );
+}
+
+String _inviteSummary(List<SentInviteStatus> shown) {
+  if (shown.isEmpty) return '';
+  final pending =
+      shown.where((invite) => invite.status != InviteStatus.accepted).length;
+  final confirmed = shown.length - pending;
+  return '$pending pendentes | $confirmed confirmados';
+}
+
+class _InviteAvatar extends StatelessWidget {
+  const _InviteAvatar(this.invite);
+
+  final SentInviteStatus invite;
+
+  @override
+  Widget build(BuildContext context) {
+    final badge = invite.status == InviteStatus.accepted
+        ? Icons.check_circle
+        : Icons.hourglass_bottom;
+    final badgeColor =
+        invite.status == InviteStatus.accepted ? Colors.green : Colors.orange;
+
+    final url = invite.friend.avatarUrl;
+    final display = invite.friend.displayName;
+
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        CircleAvatar(
+          radius: 16,
+          backgroundImage:
+              url != null && url.isNotEmpty ? NetworkImage(url) : null,
+          child: (url == null || url.isEmpty)
+              ? Text(
+                  display.isNotEmpty ? display[0].toUpperCase() : '?',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12,
+                  ),
+                )
+              : null,
+        ),
+        Positioned(
+          bottom: -2,
+          right: -2,
+          child: Container(
+            padding: const EdgeInsets.all(2),
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white,
+            ),
+            child: Icon(
+              badge,
+              color: badgeColor,
+              size: 12,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _OverlappedAvatars extends StatelessWidget {
+  const _OverlappedAvatars({required this.invites});
+
+  final List<SentInviteStatus> invites;
+
+  @override
+  Widget build(BuildContext context) {
+    if (invites.isEmpty) return const SizedBox.shrink();
+
+    final cappedCount = invites.length > 3 ? 3 : invites.length;
+    final displayInvites = invites.take(cappedCount).toList();
+    final remaining = invites.length - cappedCount;
+
+    final items = <Widget>[];
+    for (var i = 0; i < displayInvites.length; i++) {
+      items.add(Positioned(
+        left: i * 18.0,
+        child: _InviteAvatar(displayInvites[i]),
+      ));
+    }
+
+    // Final slot shows +X or empty placeholder
+    items.add(Positioned(
+      left: cappedCount * 18.0,
+      child: remaining > 0
+          ? _PlusAvatar(remaining)
+          : const _PlusAvatar(0, isEmptySlot: true),
+    ));
+
+    final totalItems = cappedCount + 1;
+    final width = totalItems * 18.0 + 16.0;
+
+    return SizedBox(
+      width: width,
+      height: 36,
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: items,
+      ),
+    );
+  }
+}
+
+class _PlusAvatar extends StatelessWidget {
+  const _PlusAvatar(this.count, {this.isEmptySlot = false});
+
+  final int count;
+  final bool isEmptySlot;
+
+  @override
+  Widget build(BuildContext context) {
+    final bgColor = isEmptySlot
+        ? Colors.grey.withValues(alpha: 0.1)
+        : Colors.grey.withValues(alpha: 0.2);
+    final borderColor = Colors.grey.withValues(alpha: 0.5);
+
+    return CircleAvatar(
+      radius: 16,
+      backgroundColor: Colors.transparent,
+      child: Container(
+        width: 32,
+        height: 32,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: bgColor,
+          border: Border.all(color: borderColor, style: BorderStyle.solid),
+        ),
+        child: Center(
+          child: isEmptySlot
+              ? Icon(Icons.person_outline,
+                  size: 16, color: Colors.grey.withValues(alpha: 0.8))
+              : Text(
+                  '+$count',
+                  style: TextStyle(
+                    color: Colors.grey.shade800,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 11,
+                  ),
+                ),
+        ),
+      ),
+    );
+  }
 }
