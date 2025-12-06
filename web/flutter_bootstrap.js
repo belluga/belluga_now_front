9c850e50aa47b7381a90f3b3f4cd23f9da32f0e5
Elton <elton@bellugasolutions.com.br>
2025-12-06 19:33:04 -0300

    Add branded splash progress bar for web bootstrap

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -7,9 +7,25 @@ var appDataJS = {
   'port': window.location.port,
 };
 
+const __progressUpdate = window.__appProgressUpdate || function () { };
+const __progressLabel = window.__appProgressLabel || function () { };
+
+__progressLabel('Carregando aplicação...');
+__progressUpdate(10);
+
 _flutter.loader.load({
   onEntrypointLoaded: async function(engineInitializer) {
+    __progressLabel('Inicializando engine...');
+    __progressUpdate(40);
+
     const appRunner = await engineInitializer.initializeEngine();
+
+    __progressLabel('Iniciando interface...');
+    __progressUpdate(70);
+
     await appRunner.runApp();
+
+    __progressLabel('Pronto!');
+    __progressUpdate(100);
   }
 });
