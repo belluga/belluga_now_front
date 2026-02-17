cd2cf0fbbed635a2aecd250fb613d8594c8abe64
Elton <elton@bellugasolutions.com.br>
2026-02-17 13:48:55 -0300

    fix(tenant-admin): normalize taxonomy term routes and disable url media fields

--- lib/presentation/tenant_admin/taxonomies/screens/tenant_admin_taxonomy_term_detail_screen.dart
+++ lib/presentation/tenant_admin/taxonomies/screens/tenant_admin_taxonomy_term_detail_screen.dart
@@ -144,6 +144,7 @@ class _TenantAdminTaxonomyTermDetailScreenState
                               TenantAdminTaxonomyTermEditRoute(
                                 taxonomyId: widget.taxonomyId,
                                 taxonomyName: widget.taxonomyName,
+                                termId: term.id,
                                 term: term,
                               ),
                             );
