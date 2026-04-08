031a18f971b426cca9989399fa134899f3156af8
Elton <elton@bellugasolutions.com.br>
2026-04-08 17:41:10 -0300

    Stop double converting event time labels

--- test/application/extensions/event_data_formating_test.dart
+++ test/application/extensions/event_data_formating_test.dart
@@ -1,81 +1,10 @@
 import 'package:belluga_now/application/extensions/event_data_formating.dart';
-import 'package:belluga_now/domain/services/timezone_service_contract.dart';
-import 'package:belluga_now/domain/services/value_objects/timezone_service_contract_values.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:get_it/get_it.dart';
 
 void main() {
-  setUp(() async {
-    await GetIt.I.reset();
-  });
-
-  tearDown(() async {
-    await GetIt.I.reset();
-  });
-
-  test('timeLabel uses timezone boundary service for utc values', () {
-    GetIt.I.registerSingleton<TimezoneServiceContract>(
-      _FakeTimezoneService(hoursOffset: -3),
-    );
-
+  test('timeLabel formats provided datetime without timezone conversion', () {
     final value = DateTime.utc(2026, 3, 29, 23, 15);
 
-    expect(value.timeLabel, '20:15');
+    expect(value.timeLabel, '23:15');
   });
-}
-
-class _FakeTimezoneService implements TimezoneServiceContract {
-  _FakeTimezoneService({required this.hoursOffset});
-
-  final int hoursOffset;
-
-  @override
-  TimezoneServiceContractDateTimeValue utcToLocal(
-    TimezoneServiceContractDateTimeValue value,
-  ) {
-    final raw = value.value;
-    final baseUtc = raw.isUtc ? raw : raw.toUtc();
-    final shifted = baseUtc.add(Duration(hours: hoursOffset));
-    return timezoneServiceDateTime(
-      DateTime(
-        shifted.year,
-        shifted.month,
-        shifted.day,
-        shifted.hour,
-        shifted.minute,
-        shifted.second,
-        shifted.millisecond,
-        shifted.microsecond,
-      ),
-      defaultValue: shifted,
-    );
-  }
-
-  @override
-  TimezoneServiceContractDateTimeValue localToUtc(
-    TimezoneServiceContractDateTimeValue value,
-  ) {
-    final raw = value.value;
-    final normalized = DateTime(
-      raw.year,
-      raw.month,
-      raw.day,
-      raw.hour,
-      raw.minute,
-      raw.second,
-      raw.millisecond,
-      raw.microsecond,
-    );
-    final utcValue = DateTime.utc(
-      normalized.year,
-      normalized.month,
-      normalized.day,
-      normalized.hour,
-      normalized.minute,
-      normalized.second,
-      normalized.millisecond,
-      normalized.microsecond,
-    ).subtract(Duration(hours: hoursOffset));
-    return timezoneServiceDateTime(utcValue, defaultValue: utcValue);
-  }
 }
